ce9625ffeaa9272a989ef4a1fce34fbfe5021a37
Ralf Drehmer Wink <ralf_wink@terceiros.sicredi.com.br>
2025-05-17 07:39:37 -0300

    first commit

---
+++ src/main/java/io/challenge/best_matched_restaurants/BestMatchedRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package io.challenge.best_matched_restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestMatchedRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestMatchedRestaurantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/challenge/best_matched_restaurants/BestMatchedRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.challenge.best_matched_restaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestMatchedRestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
