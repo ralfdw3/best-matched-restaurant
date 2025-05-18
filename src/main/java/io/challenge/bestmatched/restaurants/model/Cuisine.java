ba660c1ad08365439ea1092686c55e26601dc0a4
Ralf Drehmer Wink <ralf_wink@terceiros.sicredi.com.br>
2025-05-18 09:27:14 -0300

    feat - Adding getters to cuisine

--- src/main/java/io/challenge/bestmatched/restaurants/model/Cuisine.java
+++ src/main/java/io/challenge/bestmatched/restaurants/model/Cuisine.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class Cuisine {
     @Id
     private Long id;
