0f26aba7b864abf4c08d88d1a36139b41982540c
Ralf Drehmer Wink <ralf_wink@terceiros.sicredi.com.br>
2025-05-20 08:03:04 -0300

    feat - Adding @Repository at the repositories classes

--- src/main/java/io/challenge/bestmatched/restaurants/repository/CuisineRepository.java
+++ src/main/java/io/challenge/bestmatched/restaurants/repository/CuisineRepository.java
@@ -2,6 +2,8 @@ package io.challenge.bestmatched.restaurants.repository;
 
 import io.challenge.bestmatched.restaurants.model.Cuisine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
 }
--- src/main/java/io/challenge/bestmatched/restaurants/repository/RestaurantRepository.java
+++ src/main/java/io/challenge/bestmatched/restaurants/repository/RestaurantRepository.java
@@ -3,9 +3,11 @@ package io.challenge.bestmatched.restaurants.repository;
 import io.challenge.bestmatched.restaurants.model.Restaurant;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     List<Restaurant> findAll(final Specification<Restaurant> specification);
 }
